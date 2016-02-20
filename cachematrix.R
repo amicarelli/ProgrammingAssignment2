325c5472e439a51a939fc23226d42756b5e97055
amicarelli <amicarelli@gmail.com>
2016-02-20 15:28:47 -0600

    Matrix cache functions
    adding functions for creating and caching matrices

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache matrices operations to save computation
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat = NULL
+  inv = NULL
+  # set
+  set <- function(y){
+    mat <<- y
+    inv <<- NULL
+  }
+  get <- function() mat
+  setinv <- function(matinv) inv <<- matinv
+  getinv <- function() inv
+  list(set=set, get=get, setinv=setinv,getinv=getinv)
 }
 
 
@@ -12,4 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat,...)
+  x$setinv(inv)
+  inv
 }
